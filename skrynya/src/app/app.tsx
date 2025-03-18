28c9c673abc6d212233948fb3d463ffe46dc7efd
rasthpop <rasthetrollman@example.com>
2025-03-18 17:10:37 +0200

    basic setup for work

---
+++ skrynya/src/app/app.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function App(){
+  return(
+    <div className="ml-[260px] 2xl:ml-[360px]">
+      hello
+    </div>
+  )
+}
---
+++ skrynya/src/app/page.tsx
@@ -0,0 +1,11 @@
+import Sidebar from "@/components/sidebar";
+import App from "./app";
+
+export default function Home() {
+  return (
+    <div className="">
+      <Sidebar/>
+      <App/>
+    </div>
+  );
+}
