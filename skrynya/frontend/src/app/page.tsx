6b7e0926f0fc06c09e16b1d3ee3f45a872b8486a
rasthpop <rasthetrollman@example.com>
2025-03-31 18:31:12 +0300

    registration | login added + current main page renders registration

--- skrynya/frontend/src/app/page.tsx
+++ skrynya/frontend/src/app/page.tsx
@@ -1,15 +1,19 @@
 import Sidebar from "@/components/sidebar";
-import App from "./pages/profile/app";
+import Profile from "./pages/profile/app";
 import Header from "@/components/header";
+import Registration from "./pages/registration/app";
 
 export default function Home() {
   return (
-    <div className="">
-      <Sidebar/>
-      <div>
-        <Header/>
-        <App/>
-      </div>
-    </div>
+    // <div>
+    //   <Sidebar/>
+    //   <div>
+    //     <Header/>
+    //     {/* <Profile/> */}
+    //   </div>
+    // </div>
+  <div className="flex justify-center">
+  <Registration/>
+  </div>
   );
 }
---
+++ skrynya/frontend/src/app/pages/registration/app.tsx
@@ -0,0 +1,118 @@
+'use client'
+import React, { useState } from "react";
+
+interface FormData {
+  username: string;
+  email: string;
+  password: string;
+  confirm: string
+}
+
+interface LogFormData {
+  login: string;
+  password: string;
+}
+
+export default function Registration() {
+  const [regformData, setregFormData] = useState<FormData>({
+    username: "",
+    email: "",
+    password: "",
+    confirm: ""
+  });
+
+  const [logformData, setlogFormData] = useState<LogFormData>({
+    login: "",
+    password: "",
+  });
+
+  const [isreg, setReg] = useState(false)
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = e.target;
+    if (isreg){
+      setregFormData((prev) => ({ ...prev, [name]: value }));
+    }
+    else {
+      setlogFormData((prev) => ({ ...prev, [name]: value }));
+    }
+  };
+
+  const handleSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+    if (isreg) {
+      if (regformData.confirm == regformData.password){
+        console.log("Submitted Data:", regformData);
+      }
+      else{
+        console.log("Passwords do not match!")
+      }
+    }
+    else{
+      console.log("Submitted Data:", logformData);
+    }
+  };
+
+  return (
+    <div className="flex items-center flex-col mt-30 w-[40%] shadow-lg p-8">
+        <h2 className="text-xl font-semibold mb-4">Register</h2>
+        <form onSubmit={handleSubmit} className="flex flex-col gap-8 w-[60%]">
+          {isreg ?
+          <div className="flex flex-col gap-8">
+            <input
+              className="border-b-1 border-gray-400"
+              type="text"
+              name="username"
+              placeholder="Username"
+              value={regformData.username}
+              onChange={handleChange}
+              required
+              />
+            <input
+              className="border-b-1 border-gray-400"
+              type="email"
+              name="email"
+              placeholder="Email"
+              value={regformData.email}
+              onChange={handleChange}
+              required
+              /> 
+            </div>
+            : 
+            <input
+            className="border-b-1 border-gray-400"
+            type="text"
+            name="login"
+            placeholder="Логін"
+            value={logformData.login}
+            onChange={handleChange}
+            required
+            />
+            }
+          <input
+            className="border-b-1 border-gray-400"
+            type="password"
+            name="password"
+            placeholder="Password"
+            value={isreg ? regformData.password : logformData.password}
+            onChange={handleChange}
+            required
+          />
+          {isreg ?  <input
+            className="border-b-1 border-gray-400"
+            type="password"
+            name="confirm"
+            placeholder="Confirm password"
+            value={regformData.confirm}
+            onChange={handleChange}
+            required
+          /> : <></>}
+          <span onClick={() => (setReg(!isreg))} className="text-sm text-hiblue tracking-tight cursor-pointer">
+            {isreg ? "У мене є акаунт" : "Зареєструватися"}
+          </span>
+          <button type="submit" className="w-full bg-main text-white rounded-lg">Submit</button>
+        </form>
+      </div>
+  );
+};
+
