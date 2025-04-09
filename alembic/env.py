9c73c24f6f22c6ae6c73ec5c136fe1fc89896928
Pavlo Ruzhylo <ruzhylo.pn@ucu.edu.ua>
2025-04-09 22:08:33 +0300

    tags database migration

--- alembic/env.py
+++ alembic/env.py
@@ -1,3 +1,4 @@
+from skrynya.backend.api.database import Base, engine
 from logging.config import fileConfig
 
 from sqlalchemy import engine_from_config
@@ -8,7 +9,6 @@ from alembic import context
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
 config = context.config
-from skrynya.backend.api.database import Base, engine
 # Interpret the config file for Python logging.
 # This line sets up loggers basically.
 if config.config_file_name is not None:
