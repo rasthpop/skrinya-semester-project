4c3f79dc8d8bfebde1bae392ee70b25e81dae341
Pavlo Ruzhylo <ruzhylo.pn@ucu.edu.ua>
2025-04-09 18:13:29 +0300

    made longer duration of access token

--- skrynya/backend/api/routers/auth.py
+++ skrynya/backend/api/routers/auth.py
@@ -67,5 +67,5 @@ async def login_for_access_token(form_data: Annotated[OAuth2PasswordRequestForm,
     user = authenticate_user(form_data.username, form_data.password, db)
     if not user:
         raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED)
-    token = create_access_token(user.username, user.id, timedelta(minutes=20))
+    token = create_access_token(user.username, user.id, timedelta(days=1))
     return {'access_token': token, 'token_type': 'bearer'}
