98aa371d6db41b5708fc6fc2b2edcd186e5b1888
Pavlo Ruzhylo <ruzhylo.pn@ucu.edu.ua>
2025-04-28 14:03:16 +0300

    changed engine parameters to match postgresql

--- skrynya/backend/api/database.py
+++ skrynya/backend/api/database.py
@@ -6,7 +6,7 @@ from sqlalchemy.orm import sessionmaker
 SQL_ALCHEMY_DATABASE_URL = 'postgresql://skrynya_database_user:vAt0MiNOmjr7aaXl42ybPY5SA1NKBGD4@dpg-d07lnrqli9vc73fdsocg-a/skrynya_database'
 # SQL_ALCHEMY_DATABASE_URL = 'sqlite:///skrynia_app.db'
 
-engine = create_engine(SQL_ALCHEMY_DATABASE_URL, connect_args={'check_same_thread': False})
+engine = create_engine(SQL_ALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
